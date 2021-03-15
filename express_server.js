ac8d0cf8dba343a1c69115be4014ecc84b5d027c
Tessa Azar MacLean <tessaamac@gmail.com>
2021-03-15 21:17:48 +0000

    Added Hello <b>World</b>

--- express_server.js
+++ express_server.js
@@ -3,10 +3,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// Saved urls
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// Home page
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -14,6 +17,10 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
