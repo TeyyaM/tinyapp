52c75706431941c26531ccdee2b03745ac64c71f
Tessa Azar MacLean <tessaamac@gmail.com>
2021-03-15 23:09:44 +0000

    added urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -25,6 +25,11 @@ app.get("/urls.json", (req, res) => {
   res.render(urlDatabase);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.render("<html><body>Hello <b>World</b></body></html>\n");
 });
